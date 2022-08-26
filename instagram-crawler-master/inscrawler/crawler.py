9454e22837bec37f037413fed0ef1ce929132806
Youngin Lee <joyfuljuli@pionnet.co.kr>
2022-08-26 11:03:01 +0900

    [Update] instagram-crawler-master

--- instagram-crawler-master/inscrawler/crawler.py
+++ instagram-crawler-master/inscrawler/crawler.py
@@ -79,18 +79,16 @@ class InsCrawler(Logging):
         username = secret.username[int(index)]
         print(f'username: {username}')
         password = secret.password[int(index)]
-        print(f'password: {password}')
-        # browser = self.browser
-        # url = "%s/accounts/login/" % (InsCrawler.URL)
-        # browser.get(url)
-        # u_input = browser.find_one('input[name="username"]')
-        # u_input.send_keys(username)
-        # p_input = browser.find_one('input[name="password"]')
-        # p_input.send_keys(password)
+        browser = self.browser
+        url = "%s/accounts/login/" % InsCrawler.URL
+        browser.get(url)
+        u_input = browser.find_one('input[name="username"]')
+        u_input.send_keys(username)
+        p_input = browser.find_one('input[name="password"]')
+        p_input.send_keys(password)
 
-
-        # login_btn = browser.find_one(".L3NKy")
-        # login_btn.click()
+        login_btn = browser.find_one(".L3NKy")
+        login_btn.click()
 
         # @retry()
         # def check_login():
@@ -100,17 +98,23 @@ class InsCrawler(Logging):
         # check_login()
 
     def get_user_following(self, username):
-        # href = "/nice.english.sam/following/"
         browser = self.browser
-        url = "%s/%s/following/" % (InsCrawler.URL, username)
-        print(f'url: {url}')
+        # url = "%s/%s/following/" % (InsCrawler.URL, username)
+        url = "%s/%s/" % (InsCrawler.URL, username)
         data_set = dict()
         browser.get(url)
+
+        a_tags = browser.find("._aa_8 ul")
+        print(f'ul contents {a_tags}')
+        following_btn = a_tags[2]
+        following_btn.click()
+
+        # 크롬 브라우저를 띄워서 조작하듯이, 그래서 로드시간 고려, 네트워크 느리면 덜 수집할수도
         time.sleep(5)
         scroll = 0
-        while scroll < 5:  # scroll 5 times
+        while scroll < 10:  # scroll 10 times ?? 원래 5였음
             browser.scroll_down()
-            time.sleep(3)
+            time.sleep(3)  # 임의로 정한 시간
             scroll += 1
 
         return_val = browser.find_following()
@@ -118,7 +122,6 @@ class InsCrawler(Logging):
         data_set["url"] = str(url)
 
         return data_set
-
 
     def get_user_image(self, username):
         browser = self.browser
@@ -134,225 +137,225 @@ class InsCrawler(Logging):
         scroll = 0
         while scroll < 5:  # scroll 5 times
             browser.scroll_down()
-            time.sleep(3)
+            time.sleep(3)  # following 가져오는거랑 차이 있음(5), 일단 임의로 차이둠
             scroll += 1
             return_val = browser.find_image("N")
             for url in return_val.get('images'):
                 if url not in data_set.get('images'):
                     data_set.get('images').append(url)
 
-        # print(f"userid: {data_set.get('userid')}")
-        # print(f"usernick: {data_set.get('usernick')}")
-        # print(f"images : {data_set.get('images')}")
+        print(f"userid: {data_set.get('userid')}")
+        print(f"usernick: {data_set.get('usernick')}")
+        print(f"images : {data_set.get('images')}")
 
         return data_set
 
-    # def get_user_profile(self, username):
-    #     browser = self.browser
-    #     url = "%s/%s/" % (InsCrawler.URL, username)
-    #     print(f'url: {url}')
-    #     browser.get(url)
-    #     browser.find_username("._aacl._aacs._aact._aacx._aada h2")
-    #
-    # def get_user_profile_from_script_shared_data(self, username):
-    #     browser = self.browser
-    #     url = "%s/%s/" % (InsCrawler.URL, username)
-    #     browser.get(url)
-    #     source = browser.driver.page_source
-    #     p = re.compile(r"window._sharedData = (?P<json>.*?);</script>", re.DOTALL)
-    #     json_data = re.search(p, source).group("json")
-    #     data = json.loads(json_data)
-    #
-    #     user_data = data["entry_data"]["ProfilePage"][0]["graphql"]["user"]
-    #
-    #     return {
-    #         "name": user_data["full_name"],
-    #         "desc": user_data["biography"],
-    #         "photo_url": user_data["profile_pic_url_hd"],
-    #         "post_num": user_data["edge_owner_to_timeline_media"]["count"],
-    #         "follower_num": user_data["edge_followed_by"]["count"],
-    #         "following_num": user_data["edge_follow"]["count"],
-    #         "website": user_data["external_url"],
-    #     }
-    #
-    # def get_user_posts(self, username, number=None, detail=False):
-    #     user_profile = self.get_user_profile(username)
-    #     if not number:
-    #         number = instagram_int(user_profile["post_num"])
-    #
-    #     self._dismiss_login_prompt()
-    #
-    #     if detail:
-    #         return self._get_posts_full(number)
-    #     else:
-    #         return self._get_posts(number)
-    #
-    # def get_latest_posts_by_tag(self, tag, num):
-    #     url = "%s/explore/tags/%s/" % (InsCrawler.URL, tag)
-    #     self.browser.get(url)
-    #     return self._get_posts(num)
+    def get_user_profile(self, username):
+        browser = self.browser
+        url = "%s/%s/" % (InsCrawler.URL, username)
+        print(f'url: {url}')
+        browser.get(url)
+        browser.find_username("._aacl._aacs._aact._aacx._aada h2")
 
-    # def auto_like(self, tag="", maximum=1000):
-    #     self.login()
-    #     browser = self.browser
-    #     if tag:
-    #         url = "%s/explore/tags/%s/" % (InsCrawler.URL, tag)
-    #     else:
-    #         url = "%s/explore/" % (InsCrawler.URL)
-    #     self.browser.get(url)
-    #
-    #     ele_post = browser.find_one(".v1Nh3 a")
-    #     ele_post.click()
-    #
-    #     for _ in range(maximum):
-    #         heart = browser.find_one(".dCJp8 .glyphsSpriteHeart__outline__24__grey_9")
-    #         if heart:
-    #             heart.click()
-    #             randmized_sleep(2)
-    #
-    #         left_arrow = browser.find_one(".HBoOv")
-    #         if left_arrow:
-    #             left_arrow.click()
-    #             randmized_sleep(2)
-    #         else:
-    #             break
-    #
-    # def _get_posts_full(self, num):
-    #     @retry()
-    #     def check_next_post(cur_key):
-    #         ele_a_datetime = browser.find_one(".eo2As .c-Yi7")
-    #
-    #         # It takes time to load the post for some users with slow network
-    #         if ele_a_datetime is None:
-    #             raise RetryException()
-    #
-    #         next_key = ele_a_datetime.get_attribute("href")
-    #         if cur_key == next_key:
-    #             raise RetryException()
-    #
-    #     browser = self.browser
-    #     browser.implicitly_wait(1)
-    #     browser.scroll_down()
-    #     ele_post = browser.find_one(".v1Nh3 a")
-    #     ele_post.click()
-    #     dict_posts = {}
-    #
-    #     pbar = tqdm(total=num)
-    #     pbar.set_description("fetching")
-    #     cur_key = None
-    #
-    #     all_posts = self._get_posts(num)
-    #     i = 1
-    #
-    #     # Fetching all posts
-    #     for _ in range(num):
-    #         dict_post = {}
-    #
-    #         # Fetching post detail
-    #         try:
-    #             if(i < num):
-    #                 check_next_post(all_posts[i]['key'])
-    #                 i = i + 1
-    #
-    #             # Fetching datetime and url as key
-    #             ele_a_datetime = browser.find_one(".eo2As .c-Yi7")
-    #             cur_key = ele_a_datetime.get_attribute("href")
-    #             dict_post["key"] = cur_key
-    #             fetch_datetime(browser, dict_post)
-    #             fetch_imgs(browser, dict_post)
-    #             fetch_likes_plays(browser, dict_post)
-    #             fetch_likers(browser, dict_post)
-    #             fetch_caption(browser, dict_post)
-    #             fetch_comments(browser, dict_post)
-    #
-    #         except RetryException:
-    #             sys.stderr.write(
-    #                 "\x1b[1;31m"
-    #                 + "Failed to fetch the post: "
-    #                 + cur_key or 'URL not fetched'
-    #                 + "\x1b[0m"
-    #                 + "\n"
-    #             )
-    #             break
-    #
-    #         except Exception:
-    #             sys.stderr.write(
-    #                 "\x1b[1;31m"
-    #                 + "Failed to fetch the post: "
-    #                 + cur_key if isinstance(cur_key,str) else 'URL not fetched'
-    #                 + "\x1b[0m"
-    #                 + "\n"
-    #             )
-    #             traceback.print_exc()
-    #
-    #         self.log(json.dumps(dict_post, ensure_ascii=False))
-    #         dict_posts[browser.current_url] = dict_post
-    #
-    #         pbar.update(1)
-    #
-    #     pbar.close()
-    #     posts = list(dict_posts.values())
-    #     if posts:
-    #         posts.sort(key=lambda post: post["datetime"], reverse=True)
-    #     return posts
+    def get_user_profile_from_script_shared_data(self, username):
+        browser = self.browser
+        url = "%s/%s/" % (InsCrawler.URL, username)
+        browser.get(url)
+        source = browser.driver.page_source
+        p = re.compile(r"window._sharedData = (?P<json>.*?);</script>", re.DOTALL)
+        json_data = re.search(p, source).group("json")
+        data = json.loads(json_data)
 
-    # def _get_posts(self, num):
-    #     """
-    #         To get posts, we have to click on the load more
-    #         button and make the browser call post api.
-    #     """
-    #     TIMEOUT = 600
-    #     browser = self.browser
-    #     key_set = set()
-    #     posts = []
-    #     pre_post_num = 0
-    #     wait_time = 1
-    #
-    #     pbar = tqdm(total=num)
-    #
-    #     def start_fetching(pre_post_num, wait_time):
-    #         ele_posts = browser.find(".v1Nh3 a")
-    #         for ele in ele_posts:
-    #             key = ele.get_attribute("href")
-    #             if key not in key_set:
-    #                 dict_post = { "key": key }
-    #                 ele_img = browser.find_one(".KL4Bh img", ele)
-    #                 dict_post["caption"] = ele_img.get_attribute("alt")
-    #                 dict_post["img_url"] = ele_img.get_attribute("src")
-    #
-    #                 fetch_details(browser, dict_post)
-    #
-    #                 key_set.add(key)
-    #                 posts.append(dict_post)
-    #
-    #                 if len(posts) == num:
-    #                     break
-    #
-    #         if pre_post_num == len(posts):
-    #             pbar.set_description("Wait for %s sec" % (wait_time))
-    #             sleep(wait_time)
-    #             pbar.set_description("fetching")
-    #
-    #             wait_time *= 2
-    #             browser.scroll_up(300)
-    #         else:
-    #             wait_time = 1
-    #
-    #         pre_post_num = len(posts)
-    #         browser.scroll_down()
-    #
-    #         return pre_post_num, wait_time
-    #
-    #     pbar.set_description("fetching")
-    #     while len(posts) < num and wait_time < TIMEOUT:
-    #         post_num, wait_time = start_fetching(pre_post_num, wait_time)
-    #         pbar.update(post_num - pre_post_num)
-    #         pre_post_num = post_num
-    #
-    #         loading = browser.find_one(".W1Bne")
-    #         if not loading and wait_time > TIMEOUT / 2:
-    #             break
-    #
-    #     pbar.close()
-    #     print("Done. Fetched %s posts." % (min(len(posts), num)))
-    #     return posts[:num]
+        user_data = data["entry_data"]["ProfilePage"][0]["graphql"]["user"]
+
+        return {
+            "name": user_data["full_name"],
+            "desc": user_data["biography"],
+            "photo_url": user_data["profile_pic_url_hd"],
+            "post_num": user_data["edge_owner_to_timeline_media"]["count"],
+            "follower_num": user_data["edge_followed_by"]["count"],
+            "following_num": user_data["edge_follow"]["count"],
+            "website": user_data["external_url"],
+        }
+
+    def get_user_posts(self, username, number=None, detail=False):
+        user_profile = self.get_user_profile(username)
+        if not number:
+            number = instagram_int(user_profile["post_num"])
+
+        self._dismiss_login_prompt()
+
+        if detail:
+            return self._get_posts_full(number)
+        else:
+            return self._get_posts(number)
+
+    def get_latest_posts_by_tag(self, tag, num):
+        url = "%s/explore/tags/%s/" % (InsCrawler.URL, tag)
+        self.browser.get(url)
+        return self._get_posts(num)
+
+    def auto_like(self, tag="", maximum=1000):
+        self.login()
+        browser = self.browser
+        if tag:
+            url = "%s/explore/tags/%s/" % (InsCrawler.URL, tag)
+        else:
+            url = "%s/explore/" % (InsCrawler.URL)
+        self.browser.get(url)
+
+        ele_post = browser.find_one(".v1Nh3 a")
+        ele_post.click()
+
+        for _ in range(maximum):
+            heart = browser.find_one(".dCJp8 .glyphsSpriteHeart__outline__24__grey_9")
+            if heart:
+                heart.click()
+                randmized_sleep(2)
+
+            left_arrow = browser.find_one(".HBoOv")
+            if left_arrow:
+                left_arrow.click()
+                randmized_sleep(2)
+            else:
+                break
+
+    def _get_posts_full(self, num):
+        @retry()
+        def check_next_post(cur_key):
+            ele_a_datetime = browser.find_one(".eo2As .c-Yi7")
+
+            # It takes time to load the post for some users with slow network
+            if ele_a_datetime is None:
+                raise RetryException()
+
+            next_key = ele_a_datetime.get_attribute("href")
+            if cur_key == next_key:
+                raise RetryException()
+
+        browser = self.browser
+        browser.implicitly_wait(1)
+        browser.scroll_down()
+        ele_post = browser.find_one(".v1Nh3 a")
+        ele_post.click()
+        dict_posts = {}
+
+        pbar = tqdm(total=num)
+        pbar.set_description("fetching")
+        cur_key = None
+
+        all_posts = self._get_posts(num)
+        i = 1
+
+        # Fetching all posts
+        for _ in range(num):
+            dict_post = {}
+
+            # Fetching post detail
+            try:
+                if (i < num):
+                    check_next_post(all_posts[i]['key'])
+                    i = i + 1
+
+                # Fetching datetime and url as key
+                ele_a_datetime = browser.find_one(".eo2As .c-Yi7")
+                cur_key = ele_a_datetime.get_attribute("href")
+                dict_post["key"] = cur_key
+                fetch_datetime(browser, dict_post)
+                fetch_imgs(browser, dict_post)
+                fetch_likes_plays(browser, dict_post)
+                fetch_likers(browser, dict_post)
+                fetch_caption(browser, dict_post)
+                fetch_comments(browser, dict_post)
+
+            except RetryException:
+                sys.stderr.write(
+                    "\x1b[1;31m"
+                    + "Failed to fetch the post: "
+                    + cur_key or 'URL not fetched'
+                    + "\x1b[0m"
+                    + "\n"
+                )
+                break
+
+            except Exception:
+                sys.stderr.write(
+                    "\x1b[1;31m"
+                    + "Failed to fetch the post: "
+                    + cur_key if isinstance(cur_key, str) else 'URL not fetched'
+                                                               + "\x1b[0m"
+                                                               + "\n"
+                )
+                traceback.print_exc()
+
+            self.log(json.dumps(dict_post, ensure_ascii=False))
+            dict_posts[browser.current_url] = dict_post
+
+            pbar.update(1)
+
+        pbar.close()
+        posts = list(dict_posts.values())
+        if posts:
+            posts.sort(key=lambda post: post["datetime"], reverse=True)
+        return posts
+
+    def _get_posts(self, num):
+        """
+            To get posts, we have to click on the load more
+            button and make the browser call post api.
+        """
+        TIMEOUT = 600
+        browser = self.browser
+        key_set = set()
+        posts = []
+        pre_post_num = 0
+        wait_time = 1
+
+        pbar = tqdm(total=num)
+
+        def start_fetching(pre_post_num, wait_time):
+            ele_posts = browser.find(".v1Nh3 a")
+            for ele in ele_posts:
+                key = ele.get_attribute("href")
+                if key not in key_set:
+                    dict_post = {"key": key}
+                    ele_img = browser.find_one(".KL4Bh img", ele)
+                    dict_post["caption"] = ele_img.get_attribute("alt")
+                    dict_post["img_url"] = ele_img.get_attribute("src")
+
+                    fetch_details(browser, dict_post)
+
+                    key_set.add(key)
+                    posts.append(dict_post)
+
+                    if len(posts) == num:
+                        break
+
+            if pre_post_num == len(posts):
+                pbar.set_description("Wait for %s sec" % (wait_time))
+                sleep(wait_time)
+                pbar.set_description("fetching")
+
+                wait_time *= 2
+                browser.scroll_up(300)
+            else:
+                wait_time = 1
+
+            pre_post_num = len(posts)
+            browser.scroll_down()
+
+            return pre_post_num, wait_time
+
+        pbar.set_description("fetching")
+        while len(posts) < num and wait_time < TIMEOUT:
+            post_num, wait_time = start_fetching(pre_post_num, wait_time)
+            pbar.update(post_num - pre_post_num)
+            pre_post_num = post_num
+
+            loading = browser.find_one(".W1Bne")
+            if not loading and wait_time > TIMEOUT / 2:
+                break
+
+        pbar.close()
+        print("Done. Fetched %s posts." % (min(len(posts), num)))
+        return posts[:num]
